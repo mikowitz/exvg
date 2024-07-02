e402ecd16c1868ca41961afe2bd3a13acbae11ac
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-07-02 18:09:07 -0400

    Allow to run under Elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExVG.MixProject do
     [
       app: :exvg,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
