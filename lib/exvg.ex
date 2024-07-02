f588ffde8a6d074837b20e162de1ef32b0e7f3eb
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-07-02 18:07:46 -0400

    mix new + test setup

---
+++ lib/exvg.ex
@@ -0,0 +1,5 @@
+defmodule ExVG do
+  @moduledoc """
+  ExVG is a pure Elixir library for programatically creating SVG images.
+  """
+end
---
+++ test/exvg_test.exs
@@ -0,0 +1,4 @@
+defmodule ExVGTest do
+  use ExUnit.Case
+  doctest ExVG
+end
